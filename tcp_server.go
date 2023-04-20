8e57c35475c2740a36e0a235889bb4c65a91045e
“Computarfant” <lennartbirk@gmail.com>
2023-04-20 20:40:00 +0200

    crypt fix

--- tcp_server.go
+++ tcp_server.go
@@ -42,7 +42,7 @@ func main() {
 						return // from for loop
 					}
 
-					dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, 4)
+					dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, -4)
 					log.Println("Dekrypter melding: ", string(dekryptertMelding))
 
 					switch msg := string(dekryptertMelding); msg {
